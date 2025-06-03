823ddb16146744d5010c501c5f5ac05dde69f6ee
KUCA Brice <kuca.b@sfeir.com>
2025-06-03 19:02:27 +0200

    feat: add random op and interval

--- producer/producer.js
+++ producer/producer.js
@@ -5,37 +5,42 @@ const { exchangeDirect } = require("../constants/constants.js");
 
 const rabbitmqUrl = process.env.RABBITMQ_URL;
 
-const args = process.argv.slice(2);
-const routingKey = args[0] || "mul";
+const operations = ["add", "sub", "mul", "div", "all"];
 
-function generateMessage() {
+function generateMessage(op) {
   const n1 = Math.floor(Math.random() * 100);
   const n2 = Math.floor(Math.random() * 100);
-  return JSON.stringify({ n1, n2, op: routingKey });
+  return JSON.stringify({ n1, n2, op });
 }
 
-async function send(msg, key) {
+function getRandomOperation() {
+  return operations[Math.floor(Math.random() * operations.length)];
+}
+
+function getRandomInterval() {
+  return Math.floor(Math.random() * 4000) + 1000;
+}
+
+async function send() {
   try {
     const conn = await amqplib.connect(rabbitmqUrl);
     const channel = await conn.createChannel();
 
     await channel.assertExchange(exchangeDirect, "direct", { durable: false });
 
-    channel.publish(exchangeDirect, key, Buffer.from(msg));
-    console.log(
-      `[✓] Message envoyé avec clé '${key}' sur l'échange '${exchangeDirect}' direct`,
-      msg
-    );
+    const publishRandomMessage = () => {
+      const op = getRandomOperation();
+      const message = generateMessage(op);
+      channel.publish(exchangeDirect, op, Buffer.from(message));
+      `[✓] Message envoyé avec clé '${op}' sur l'échange '${exchangeDirect}'`,
+        setTimeout(publishRandomMessage, getRandomInterval());
+    };
 
-    setTimeout(() => {
-      conn.close();
-      process.exit(0);
-    }, 500);
+    publishRandomMessage();
   } catch (error) {
     console.error("Erreur lors de l’envoi du message:", error);
     process.exit(1);
   }
 }
 
-const message = generateMessage();
-send(message, routingKey);
+send();
